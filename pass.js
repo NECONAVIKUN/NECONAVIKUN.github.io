c10fd3446af4d5b36b079d07242e94b14c17350e
ねこナビくん <komukun7@gmail.com>
2026-05-02 07:54:04 +0900

    Update pass.js

--- pass.js
+++ pass.js
@@ -4,6 +4,7 @@ document.getElementById("go").addEventListener("click", function() {
   if (pwInput) {
     if (["ikura"].includes(pwInput)) {
       window.location.href = "/secret/" + pwInput + "/";
+    } else if{ window.location.href = "/secret/" + pwInput + "/";
     } else { alert("パスワードが違う！"); }
   } else { alert("パスワードを入れてね！"); }
 });
