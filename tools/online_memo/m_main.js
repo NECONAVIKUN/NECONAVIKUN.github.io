6b9b9aac877360ec2aa7f33a373e18c25d879263
miikun95 <miikun95@miikun95.net>
2026-05-23 15:29:38 +0900

    Fix variable declarations for URL and anchor element

--- tools/online_memo/m_main.js
+++ tools/online_memo/m_main.js
@@ -3,8 +3,8 @@ document.getElementById("exbutton").addEventListener("click", function() {
   var text = document.getElementById("input").value;
   var title = document.getElementById("title").Value;
   var blob = new Blob([text], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
   a.href = url;
   a.download = title + ".txt";
   document.body.appendChild(a);
