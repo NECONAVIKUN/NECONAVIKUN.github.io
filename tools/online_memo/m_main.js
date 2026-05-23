4e7c7ff34101c001ffadddf22480e6807250b5c6
miikun95 <miikun95@miikun95.net>
2026-05-23 15:28:48 +0900

    Refactor event listener and variable declarations

--- tools/online_memo/m_main.js
+++ tools/online_memo/m_main.js
@@ -1,11 +1,13 @@
-document.getElementById('exbutton').addEventListener("click",()=> {
-  const text = document.getElementById('input').value;
-  const title = document.getElementById('title').Value;
-  const blob = new Blob([text], { type: 'text/plain' });
+"use strict";
+document.getElementById("exbutton").addEventListener("click", function() {
+  var text = document.getElementById("input").value;
+  var title = document.getElementById("title").Value;
+  var blob = new Blob([text], { type: "text/plain" });
   const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
+  const a = document.createElement("a");
   a.href = url;
-  a.download = title+'.txt';
+  a.download = title + ".txt";
+  document.body.appendChild(a);
   a.click();
   URL.revokeObjectURL(url);
 });
