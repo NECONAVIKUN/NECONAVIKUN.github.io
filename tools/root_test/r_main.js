a30cf00f6fa21f10c0e8b57c7350d3bc78b8e11c
miikun95 <miikun95@miikun95.net>
2026-05-24 22:01:35 +0900

    Update r_main.js

--- tools/root_test/r_main.js
+++ tools/root_test/r_main.js
@@ -3,7 +3,7 @@ var quiz1;
 var quiz2;
 var q1;
 var q2;
-var quizHTML = document.getElementById("quiz").innerHTML;
+var quizElem = document.getElementById("quiz");
 var button1 = document.getElementById("m_button");
 var button2 = document.getElementById("<button");
 var button3 = document.getElementById(">button");
@@ -21,7 +21,7 @@ function correct () {
   alert("正解！");
 }
 function incorrect() {
-  quizHTML = `      <div class="main_text">
+  quizElem.innerHTML = `      <div class="main_text">
             <h2 id="text1">残念</h2>
             <h2 id="text2">　</h2>
       </div>`;
@@ -32,97 +32,56 @@ function incorrect() {
   button1.textContent = "次の問題へ";
   alert("不正解！");
 }
-function create_quiz () {
-  var rand1 = null;
-  var rand2 = null;
-  var rand3 = null;
-  var rand4 = null;
-  var rand5 = null;
-  var rand6 = null;
+function getRandomInt() {
   if (window.crypto) {
-    rand1 = window.crypto.getRandomValues(new Uint8Array(1))[0] & 1;
-    rand3 = window.crypto.getRandomValues(new Uint8Array(1))[0] & 1;
-    rand5 = window.crypto.getRandomValues(new Uint8Array(1))[0] & 1;
-    rand6 = window.crypto.getRandomValues(new Uint8Array(1))[0] & 1;
-    if (rand1 === "1") {
-      if (rand5 === "1") {
-        rand2 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-        quiz1 = "-√" + rand2;
-        q1 = -1 * rand2;
-      } else {
-        rand2 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-        quiz1 = "√" + rand2;
-        q1 = rand2;
-      }
-    } else if (rand5 === "1") {
-      rand2 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-      quiz1 = "-" + rand2;
-      q1 = -1 * rand2 * rand2;
-    } else {
-      rand2 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-      quiz1 = rand2;
-      q1 = rand2 * rand2;
-    } else if (rand3 === "1") {
-      if (rand6 === "1") {
-        rand4 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-        quiz2 = "-√" + rand4;
-        q2 = -1 * rand4;
-      } else {
-        rand4 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-        quiz2 = "√" + rand4;
-        q2 = rand4;
-      }
-    } else if (rand6 === "1") {
-      rand4 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-      quiz2 = "-" + rand4;
-      q2 = -1 * rand4 * rand4;
-    } else {
-      rand4 = (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
-      quiz2 = rand4;
-      q2 = rand4 * rand4;
-    }
+    return (window.crypto.getRandomValues(new Uint32Array(1))[0] % 99) + 1;
   } else {
-    rand1 = Math.round(Math.random());
-    rand3 = Math.round(Math.random());
-    rand5 = Math.round(Math.random());
-    rand6 = Math.round(Math.random());
-    if (rand1 === "1") {
-      if (rand5 === "1") {
-        rand2 = Math.floor(Math.random() * 99) + 1;
-        quiz1 = "-√" + rand2;
-        q1 = -1 * rand2;
-      } else {
-        rand2 = Math.floor(Math.random() * 99) + 1;
-        quiz1 = "√" + rand2;
-        q1 = rand2;
-      }
-    } else if (rand5 === "1") {
-      rand2 = Math.floor(Math.random() * 99) + 1;
-      quiz1 = "-" + rand2;
-      q1 = -1 * rand2 * rand2;
+    return Math.floor(Math.random() * 99) + 1;
+  }
+}
+function getRandomBin() {
+  if (window.crypto) {
+    return window.crypto.getRandomValues(new Uint8Array(1))[0] & 1;
+  } else {
+    return Math.round(Math.random());
+  }
+}
+function create_quiz () {
+  var q1Root = getRandomBin();
+  var q1Minus = getRandomBin();
+  var q1Num = getRandomInt();
+  var q2Root = getRandomBin();
+  var q2Minus = getRandomBin();
+  var q2Num = getRandomInt();
+  if (q1Root === "1") {
+    if (q1Minus === "1") {
+      quiz1 = "-√" + q1Num;
+      q1 = -1 * q1Num;
     } else {
-      rand2 = Math.floor(Math.random() * 99) + 1;
-      quiz1 = rand2;
-      q1 = rand2 * rand2;
-    } else if (rand3 === "1") {
-      if (rand6 === "1") {
-        rand4 = Math.floor(Math.random() * 99) + 1;
-        quiz2 = "-√" + rand4;
-        q2 = -1 * rand4;
-      } else {
-        rand4 = Math.floor(Math.random() * 99) + 1;
-        quiz2 = "√" + rand4;
-        q2 = rand4;
-      }
-    } else if (rand6 === "1") {
-      rand4 = Math.floor(Math.random() * 99) + 1;
-      quiz2 = "-" + rand4;
-      q2 = -1 * rand4 * rand4;
-    } else {
-      rand4 = Math.floor(Math.random() * 99) + 1;
-      quiz2 = rand4;
-      q2 = rand4 * rand4;
+      quiz1 = "√" + q1Num;
+      q1 = q1Num;
     }
+  } else if (q1Minus === "1") {
+    quiz1 = "-" + q1Num;
+    q1 = -1 * q1Num * q1Num;
+  } else {
+    quiz1 = q1Num;
+    q1 = q1Num * q1Num;
+  }
+  if (q2Root === "1") {
+    if (q2Minus === "1") {
+      quiz2 = "-√" + q2Num;
+      q2 = -1 * q2Num;
+    } else {
+      quiz2 = "√" + q2Num;
+      q2 = q2Num;
+    }
+  } else if (q2Minus === "1") {
+    quiz2 = "-" + q2Num;
+    q2 = -1 * q2Num * q2Num;
+  } else {
+    quiz2 = q2Num;
+    q2 = q2Num * q2Num;
   }
 }
 button1.addEventListener("click", function() {
@@ -132,7 +91,7 @@ button1.addEventListener("click", function() {
     button3.removeAttribute("class");
     button4.setAttribute("class", "hidden");
     create_quiz();
-    quizHTML = `            <div class="main_text">
+    quizElem.innerHTML = `            <div class="main_text">
                 <h2 id="text1">どっちが大きい?</h2>
                 <h2 id="text2">${quiz1}　${quiz2}</h2>
     </div>`;
@@ -142,7 +101,7 @@ button1.addEventListener("click", function() {
     button3.removeAttribute("class");
     button4.setAttribute("class", "hidden");
     create_quiz();
-    quizHTML = `        <div class="main_text">
+    quizElem.innerHTML = `        <div class="main_text">
                 <h2 id="text1">どっちが大きい?</h2>
                 <h2 id="text2">${quiz1}　${quiz2}</h2>
             </div>`;
@@ -168,7 +127,7 @@ button4.addEventListener("click", function() {
   button3.setAttribute("class", "hidden");
   button4.setAttribute("class", "hidden");
   button1.textContent = "スタート！"
-  quizHTML = `      <div class="main_text">
+  quizElem.innerHTML = `      <div class="main_text">
             <h2 id="text1">JavaScript式√どっちが大きい?</h2>
             <h2 id="text2">スタートボタンを押してね！</h2>
       </div>`;
